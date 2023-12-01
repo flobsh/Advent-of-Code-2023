67b740df5f9ccafe66588b5d06fffd50b3068a72
Florian Bescher <florian.bescher22@gmail.com>
2023-12-01 15:25:37 +0100

    feat: add io::error handling in crate's Error type

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,31 @@
 use std::fmt;
 
+#[derive(Debug)]
 pub enum Error {
     GenericError,
+    IoError(std::io::Error),
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::GenericError => write!(f, "Generic error"),
+            Self::IoError(err) => err.fmt(f),
         }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::GenericError => None,
+            Self::IoError(err) => Some(err),
+        }
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::IoError(value)
     }
 }
