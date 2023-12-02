f433acd34d1ce0905bde80957e7de4ccc00b80d5
Florian Bescher <florian.bescher22@gmail.com>
2023-12-02 18:58:48 +0100

    day01: complete part 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,5 +1,10 @@
 use aoc_2023;
 
+const PATTERNS: [&str; 20] = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1", "2",
+    "3", "4", "5", "6", "7", "8", "9",
+];
+
 struct Day01;
 
 impl Day01 {
@@ -8,6 +13,26 @@ impl Day01 {
         let last_digit = input.chars().rev().flat_map(|c| c.to_digit(10)).next();
 
         Some((first_digit? * 10 + last_digit?) as usize)
+    }
+
+    fn str_literal_to_double_digit_number(input: &str) -> Option<usize> {
+        let first_digit = (0..input.len()).find_map(|i| {
+            PATTERNS
+                .iter()
+                .enumerate()
+                .find(|(_, &number)| input[i..].starts_with(number))
+                .map(|(j, _)| j % 10)
+        });
+
+        let last_digit = (0..input.len()).rev().find_map(|i| {
+            PATTERNS
+                .iter()
+                .enumerate()
+                .find(|(_, &number)| input[i..].starts_with(number))
+                .map(|(j, _)| j % 10)
+        });
+
+        Some(first_digit? * 10 + last_digit?)
     }
 }
 
@@ -28,7 +53,12 @@ impl aoc_2023::Day for Day01 {
     }
 
     fn part_2(input: &Self::ParsedInput) -> Result<Self::Output2, aoc_2023::Error> {
-        todo!()
+        input
+            .lines()
+            .map(|line| {
+                Self::str_literal_to_double_digit_number(line).ok_or(aoc_2023::Error::GenericError)
+            })
+            .sum()
     }
 }
 
@@ -46,5 +76,11 @@ mod tests {
     fn part_1() {
         let input = std::fs::read_to_string("inputs/tests/day01_p1.txt").unwrap();
         assert_eq!(Day01::part_1(&input).unwrap(), 142)
+    }
+
+    #[test]
+    fn part_2() {
+        let input = std::fs::read_to_string("inputs/tests/day01_p2.txt").unwrap();
+        assert_eq!(Day01::part_2(&input).unwrap(), 281)
     }
 }
